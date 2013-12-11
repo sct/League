db6c91369244d96d9a037809957d22ee9bd9b698
sct <sctsnipe@gmail.com>
2013-12-11 11:54:06 -0500

    Switched to spaces instead of tabs (PSR-2 Compliance)

--- src/sct/League/Common/GameType.php
+++ src/sct/League/Common/GameType.php
@@ -3,12 +3,12 @@
 namespace sct\League\Common;
 
 class GameType {
-	const AramUnranked5x5 	= "AramUnranked5x5";
-	const CoopVsAI 			= "CoopVsAI";
-	const OdinUnranked		= "OdinUranked";
-	const RankedSolo5x5		= "RankedSolo5x5";
-	const RankedTeam3x3		= "RankedTeam3x3";
-	const RankedTeam5x5		= "RankedTeam5x5";
-	const Unranked 			= "Unranked";
-	const Unranked3x3		= "Unranked3x3";
+    const AramUnranked5x5 = "AramUnranked5x5";
+    const CoopVsAI = "CoopVsAI";
+    const OdinUnranked = "OdinUranked";
+    const RankedSolo5x5 = "RankedSolo5x5";
+    const RankedTeam3x3 = "RankedTeam3x3";
+    const RankedTeam5x5 = "RankedTeam5x5";
+    const Unranked = "Unranked";
+    const Unranked3x3 = "Unranked3x3";
 }
