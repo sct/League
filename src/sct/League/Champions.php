f8586d70c5b428f75d8bebcf509ef5146f012284
sct <sctsnipe@gmail.com>
2013-12-11 13:27:30 -0500

    Revert "Switched to spaces instead of tabs (PSR-2 Compliance)"
    This reverts commit db6c91369244d96d9a037809957d22ee9bd9b698.

--- src/sct/League/Champions.php
+++ src/sct/League/Champions.php
@@ -5,86 +5,85 @@ namespace sct\League;
 use sct\League\Exception\ChampionDoesNotExistException;
 
 class Champions {
-    /**
-     * Array containing all the currently loaded champions
-     * 
-     * @var array
-     */
-    private static $champions = array();
+	/**
+	 * Array containing all the currently loaded champions
+	 * 
+	 * @var array
+	 */
+	private static $champions = array();
 
-    /**
-     * Boolean to track whether or not champions are currently loaded
-     * 
-     * @var boolean
-     */
-    private static $isLoaded = false;
+	/**
+	 * Boolean to track whether or not champions are currently loaded
+	 * 
+	 * @var boolean
+	 */
+	private static $isLoaded = false;
 
-    /**
-     * Loads champions from the API into the champions array
-     * @param  string $region Region to load from
-     * @param  string $key    API Key
-     */
-    public static function loadChampions($region, $key)
-    {
-        $league = new League($key, $region);
-        $champions = $league->getChampions();
+	/**
+	 * Loads champions from the API into the champions array
+	 * @param  string $region Region to load from
+	 * @param  string $key    API Key
+	 */
+	public static function loadChampions($region, $key)
+	{
+		$league = new League($key, $region);
+		$champions = $league->getChampions();
 
-        foreach ($champions['champions'] as $champion) {
-            self::addChampion($champion);
-        }
-        self::$isLoaded = true;
-    }
+		foreach ($champions['champions'] as $champion) {
+			self::addChampion($champion);
+		}
+		self::$isLoaded = true;
+	}
 
-    /**
-     * Adds a champion into the champions Array
-     * 
-     * @param Array $champion Champion Array
-     */
-    public static function addChampion($champion)
-    {
-        $cObject = new Champion($champion['id'], $champion['name']);
-        $cObject->setActive($champion['active'])
-                ->setBotEnabled($champion['botEnabled'])
-                ->setBotMmEnabled($champion['botMmEnabled'])
-                ->setRanks($champion['attackRank'], $champion['defenseRank'], $champion['magicRank'], $champion['difficultyRank'])
-                ->setRankedPlayEnabled($champion['rankedPlayEnabled']);
-        self::$champions[$champion['name']] = $cObject;
-    }
+	/**
+	 * Adds a champion into the champions Array
+	 * 
+	 * @param Array $champion Champion Array
+	 */
+	public static function addChampion($champion)
+	{
+		$cObject = new Champion($champion['id'], $champion['name']);
+		$cObject->setActive($champion['active'])
+				->setBotEnabled($champion['botEnabled'])
+				->setBotMmEnabled($champion['botMmEnabled'])
+				->setRanks($champion['attackRank'], $champion['defenseRank'], $champion['magicRank'], $champion['difficultyRank'])
+				->setRankedPlayEnabled($champion['rankedPlayEnabled']);
+		self::$champions[$champion['name']] = $cObject;
+	}
 
-    /**
-     * Returns the current array of champions
-     * 
-     * @return Array Champions array
-     */
-    public static function getChampions()
-    {
-        return self::$champions;
-    }
+	/**
+	 * Returns the current array of champions
+	 * 
+	 * @return Array Champions array
+	 */
+	public static function getChampions()
+	{
+		return self::$champions;
+	}
 
-    /**
-     * Request a specific champion from the champions array. Throws an exception
-     * if it does not exist.
-     * 
-     * @param  string $name Champion Name
-     * 
-     * @return array       Champion Array
-     */
-    public static function getChampion($name)
-    {
-        if (!array_key_exists($name, self::$champions)) {
-            throw new ChampionDoesNotExistException('Champion does not exist');
-        }
+	/**
+	 * Request a specific champion from the champions array. Throws an exception
+	 * if it does not exist.
+	 * 
+	 * @param  string $name Champion Name
+	 * 
+	 * @return array       Champion Array
+	 */
+	public static function getChampion($name)
+	{
+		if (!array_key_exists($name, self::$champions))
+			throw new ChampionDoesNotExistException('Champion does not exist');
 
-        return self::$champions[$name];
-    }
+		return self::$champions[$name];
+	}
 
-    /**
-     * Returns true if there is an actively loaded champions array
-     * 
-     * @return boolean
-     */
-    public static function isLoaded()
-    {
-        return self::$isLoaded;
-    }
+	/**
+	 * Returns true if there is an actively loaded champions array
+	 * 
+	 * @return boolean
+	 */
+	public static function isLoaded()
+	{
+		return self::$isLoaded;
+	}
 }
--- src/sct/League/Summoner.php
+++ src/sct/League/Summoner.php
@@ -7,217 +7,217 @@ use sct\League\Exception\ChampionDoesNotExistException;
 
 class Summoner {
 
-    /**
-     * Instance of the League object. Used for base API calls.
-     * 
-     * @var League
-     */
-    private $league;
+	/**
+	 * Instance of the League object. Used for base API calls.
+	 * 
+	 * @var League
+	 */
+	private $league;
 
-    /**
-     * Summoner Id
-     * 
-     * @var integer
-     */
-    private $id;
+	/**
+	 * Summoner Id
+	 * 
+	 * @var integer
+	 */
+	private $id;
 
-    /**
-     * Summoner Name
-     * 
-     * @var string
-     */
-    private $name;
+	/**
+	 * Summoner Name
+	 * 
+	 * @var string
+	 */
+	private $name;
 
-    /**
-     * Summoner Profile Icon ID
-     * 
-     * @var integer
-     */
-    private $profileIconId;
+	/**
+	 * Summoner Profile Icon ID
+	 * 
+	 * @var integer
+	 */
+	private $profileIconId;
 
-    /**
-     * Summoner Level
-     * 
-     * @var integer
-     */
-    private $summonerLevel;
+	/**
+	 * Summoner Level
+	 * 
+	 * @var integer
+	 */
+	private $summonerLevel;
 
-    /**
-     * Summoner Revision Date
-     * 
-     * @var integer
-     */
-    private $revisionDate;
+	/**
+	 * Summoner Revision Date
+	 * 
+	 * @var integer
+	 */
+	private $revisionDate;
 
-    /**
-     * Summoner revision date represented as a string
-     * 
-     * @var string
-     */
-    private $revisionDateStr;
+	/**
+	 * Summoner revision date represented as a string
+	 * 
+	 * @var string
+	 */
+	private $revisionDateStr;
 
-    /**
-     * Create a new instance of the Summoner class
-     * 
-     * @param string $name   Summoner Name
-     * @param string $region Region to search in
-     * @param string $key    League API Key
-     */
-    public function __construct($name, $region, $key)
-    {
-        $this->league = new League($key, $region);
-        $summoner = $this->league->getSummonerByName($name);
-        $this->id = $summoner['id'];
-        $this->name = $summoner['name'];
-        $this->profileIconId = $summoner['profileIconId'];
-        $this->summonerLevel = $summoner['summonerLevel'];
-        $this->revisionDate = $summoner['revisionDate'];
-        $this->revisionDateStr = $summoner['revisionDateStr'];
-    }
+	/**
+	 * Create a new instance of the Summoner class
+	 * 
+	 * @param string $name   Summoner Name
+	 * @param string $region Region to search in
+	 * @param string $key    League API Key
+	 */
+	public function __construct($name, $region, $key)
+	{
+		$this->league = new League($key, $region);
+		$summoner = $this->league->getSummonerByName($name);
+		$this->id = $summoner['id'];
+		$this->name = $summoner['name'];
+		$this->profileIconId = $summoner['profileIconId'];
+		$this->summonerLevel = $summoner['summonerLevel'];
+		$this->revisionDate = $summoner['revisionDate'];
+		$this->revisionDateStr = $summoner['revisionDateStr'];
+	}
 
-    /**
-     * Returns the Summoner ID
-     * 
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+	/**
+	 * Returns the Summoner ID
+	 * 
+	 * @return integer
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
 
-    /**
-     * Returns the Summoner Name
-     * 
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
+	/**
+	 * Returns the Summoner Name
+	 * 
+	 * @return string
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
 
-    /**
-     * Returns the Summoner level
-     * 
-     * @return integer
-     */
-    public function getSummonerLevel()
-    {
-        return $this->summonerLevel;
-    }
+	/**
+	 * Returns the Summoner level
+	 * 
+	 * @return integer
+	 */
+	public function getSummonerLevel()
+	{
+		return $this->summonerLevel;
+	}
 
-    /**
-     * Returns the raw Stats response from the API
-     * 
-     * @param  string $type Type of stats to request. (summary or ranked)
-     * 
-     * @return Array       Array of the stats object
-     */
-    public function getStats($type = "summary")
-    {
-        return $this->league->getSummonerStats($this->id, $type);
-    }
+	/**
+	 * Returns the raw Stats response from the API
+	 * 
+	 * @param  string $type Type of stats to request. (summary or ranked)
+	 * 
+	 * @return Array       Array of the stats object
+	 */
+	public function getStats($type = "summary")
+	{
+		return $this->league->getSummonerStats($this->id, $type);
+	}
 
-    /**
-     * Gets the stats for the requested gametype.
-     * 
-     * @param  GameType $gametype GameType to request stats for
-     * 
-     * @return Array
-     */
-    public function getStatsForGameType($gametype = GameType::AramUnranked5x5)
-    {
-        $stats = $this->getStats();
+	/**
+	 * Gets the stats for the requested gametype.
+	 * 
+	 * @param  GameType $gametype GameType to request stats for
+	 * 
+	 * @return Array
+	 */
+	public function getStatsForGameType($gametype = GameType::AramUnranked5x5)
+	{
+		$stats = $this->getStats();
 
-        foreach ($stats['playerStatSummaries'] as $summary) {
-            if ($summary['playerStatSummaryType'] == $gametype) {
-                return $summary;
-            }
-        }
+		foreach ($stats['playerStatSummaries'] as $summary) {
+			if ($summary['playerStatSummaryType'] == $gametype) {
+				return $summary;
+			}
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    /**
-     * Gets full array of ranked stats
-     * 
-     * @return Array
-     */
-    public function getRankedStats()
-    {
-        return $this->getStats("ranked");
-    }
+	/**
+	 * Gets full array of ranked stats
+	 * 
+	 * @return Array
+	 */
+	public function getRankedStats()
+	{
+		return $this->getStats("ranked");
+	}
 
-    /**
-     * Return Array of requested champions ranked stats
-     * 
-     * @param  integer $name Champion ID
-     * 
-     * @return Array       Array of Champions ranked stats
-     */
-    public function getStatsForChampion($id)
-    {
-        $stats = $this->getRankedStats();
+	/**
+	 * Return Array of requested champions ranked stats
+	 * 
+	 * @param  integer $name Champion ID
+	 * 
+	 * @return Array       Array of Champions ranked stats
+	 */
+	public function getStatsForChampion($id)
+	{
+		$stats = $this->getRankedStats();
 
-        foreach ($stats['champions'] as $champion) {
-            if (isset($champion['id']) && $champion['id'] == $id) {
-                return $champion;
-            }
-        }
+		foreach ($stats['champions'] as $champion) {
+			if (isset($champion['id']) && $champion['id'] == $id) {
+				return $champion;
+			}
+		}
 
-        throw new ChampionDoesNotExistException('Champion not in summoners history');
-    }
+		throw new ChampionDoesNotExistException('Champion not in summoners history');
+	}
 
-    /**
-     * Requests stats for a champion by name. Uses the Champion class to make sure the champion
-     * exists and is valid. Then calls the champion up by ID.
-     * 
-     * @param  string $name Champion Name
-     * 
-     * @return array       Champion stats
-     */
-    public function getStatsForChampionByName($name)
-    {
-        if (!Champions::isLoaded()) {
-            Champions::loadChampions($this->league->getRegion(), $this->league->getKey());
-        }
+	/**
+	 * Requests stats for a champion by name. Uses the Champion class to make sure the champion
+	 * exists and is valid. Then calls the champion up by ID.
+	 * 
+	 * @param  string $name Champion Name
+	 * 
+	 * @return array       Champion stats
+	 */
+	public function getStatsForChampionByName($name)
+	{
+		if (!Champions::isLoaded()) {
+			Champions::loadChampions($this->league->getRegion(), $this->league->getKey());
+		}
 
-        try {
-            $champion = Champions::getChampion($name);
-            return $this->getStatsForChampion($champion->getId());
-        } catch (ChampionDoesNotExistException $e) {
-            return $e->getMessage();
-        }
-    }
+		try {
+			$champion = Champions::getChampion($name);
+			return $this->getStatsForChampion($champion->getId());
+		} catch (ChampionDoesNotExistException $e) {
+			return $e->getMessage();
+		}
+	}
 
-    /**
-     * Request the current match history. Max 10 results.
-     * @return array Match History
-     */
-    public function getMatchHistory()
-    {
-        $matchHistory = $this->league->getMatchHistory($this->id);
-        return $matchHistory;
-    }
+	/**
+	 * Request the current match history. Max 10 results.
+	 * @return array Match History
+	 */
+	public function getMatchHistory()
+	{
+		$matchHistory = $this->league->getMatchHistory($this->id);
+		return $matchHistory;
+	}
 
-    /**
-     * Request all masteries for this summoner
-     * 
-     * @return Array Array of Masteries
-     */
-    public function getMasteries()
-    {
-        $masteries = $this->league->getSummonerMastery($this->id);
-        return $masteries;
-    }
+	/**
+	 * Request all masteries for this summoner
+	 * 
+	 * @return Array Array of Masteries
+	 */
+	public function getMasteries()
+	{
+		$masteries = $this->league->getSummonerMastery($this->id);
+		return $masteries;
+	}
 
-    /**
-     * Request all runes for this summoner
-     * 
-     * @return Array Array of Runes
-     */
-    public function getRunes()
-    {
-        $runes = $this->league->getSummonerRunes($this->id);
-        return $runes;
-    }
+	/**
+	 * Request all runes for this summoner
+	 * 
+	 * @return Array Array of Runes
+	 */
+	public function getRunes()
+	{
+		$runes = $this->league->getSummonerRunes($this->id);
+		return $runes;
+	}
 }
